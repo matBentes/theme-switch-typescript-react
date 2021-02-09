b6f6e778b161c168480e643dbb0916e54d9fedc1
Mateus Bentes <mateus1337bentes@gmail.com>
2021-02-09 16:17:15 -0400

    initial commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,30 @@
+import React, { useContext } from 'react';
+import Switch from 'react-switch'
+import {ThemeContext} from 'styled-components'
+import { Container } from './styles';
+
+interface Props {
+  toggleTheme(): void; 
+}
+
+const Header: React.FC<Props> = ({ toggleTheme }) => {
+  const { colors, title } = useContext(ThemeContext);
+  return (
+    <Container>
+      Hello World
+      <Switch
+      onChange={toggleTheme}
+      checked={title === 'dark'}
+      checkedIcon={false}
+      uncheckedIcon={false}
+      height={10}
+      width={40}
+      handleDiameter={20}
+      offColor={colors.secondary}
+      onColor={colors.primary }
+      />
+    </Container>
+  );
+}
+
+export default Header;
---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,9 @@
+export default {
+  title: "dark",
+  colors: {
+    primary: "#C62E65",
+    secondary: "#D63AF9",
+    background: "#222",
+    text: "#fff",
+  },
+};
